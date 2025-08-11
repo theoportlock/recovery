03cc96c98061aff67d55fe8888b78606a6d557c9
theoportlock <theo.portlock@auckland.ac.nz>
2025-08-12 08:57:34 +1200

    Manuscript updates

--- code/sustained_recovery.py
+++ code/sustained_recovery.py
@@ -3,12 +3,13 @@
 
 import pandas as pd
 
-df = pd.read_csv('results/filtered/anthro.tsv', sep='\t', index_col=0)
+df = pd.read_csv('results/timepoints/yr2/anthro.tsv', sep='\t', index_col=0)
 meta = pd.read_csv('results/filtered/meta.tsv', sep='\t', index_col=0)
 
-t2 = df.query('(sampleID.str.contains("_52")) & (sampleID.str.contains("LCC1"))', engine='python')
+# Just recovered
+recovered = meta.loc[meta.Recovery == 'Recovered']
+sus_recovered = df.loc[recovered.index, 'WLZ_WHZ'].gt(-1)
+meta['Sus_recovered'] = sus_recovered
 
-t2 = t2.loc[t2.WLZ_WHZ.gt(-1)]
 
-t2.index  = t2.index.str.replace('_52', '', regex=False)
-t2['sus_recovery'] = True
+
